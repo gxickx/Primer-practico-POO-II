fc3ed18e88543a38641a57200938e2ea1bb00e51
gxickx <mgrokoe@gmail.com>
2025-10-03 19:05:04 -0300

    Cree la clase abstracta Usuario y agregué los setters y getters

---
+++ src/main/java/modelo/Usuario.java
@@ -0,0 +1,47 @@
+package modelo;
+
+abstract class Usuario {
+
+    private String nombreUsuario;
+    private String contrasenia;
+    private String correo;
+    private String nombre;
+    private String apellido;
+
+    // getters y setters
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+}
